aba760db76c63a3c750bd6a221819ad90328664e
Irinaa <lashuhina.ira@gmail.com>
2025-05-01 14:41:07 +0300

    delet gener in test_get_books_gener

--- tests.py
+++ tests.py
@@ -86,7 +86,6 @@ class TestBooksCollector:
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-        collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Комедии')
         books_genre = collector.get_books_genre()
         assert books_genre == {
             'Гордость и предубеждение и зомби': 'Фантастика',
